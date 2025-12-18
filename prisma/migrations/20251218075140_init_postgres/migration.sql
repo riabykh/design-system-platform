0c056b70147e01a748f8139af5a2d49ee9d359ee
Kyrylo Riabykh <kyryloriabykh@MacBookPro.home>
2025-12-18 08:51:47 +0100

    chore: cleanup sqlite and init postgres migration

--- prisma/migrations/20251218075140_init_postgres/migration.sql
+++ prisma/migrations/20251218075140_init_postgres/migration.sql
@@ -1,6 +1,6 @@
 -- CreateTable
 CREATE TABLE "FigmaFile" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "featureName" TEXT NOT NULL,
     "fileName" TEXT,
     "figmaUrl" TEXT NOT NULL,
@@ -8,6 +8,8 @@ CREATE TABLE "FigmaFile" (
     "category" TEXT NOT NULL,
     "icon" TEXT NOT NULL,
     "isFrequentlyUsed" BOOLEAN NOT NULL DEFAULT false,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "order" INTEGER NOT NULL DEFAULT 0
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "order" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "FigmaFile_pkey" PRIMARY KEY ("id")
 );
