15416c5d84470bcd415617aa6b208d705938e0ff
Kyrylo Riabykh <kyryloriabykh@MacBookPro.home>
2025-12-18 08:23:44 +0100

    fix: use correct datasources config for Prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -5,7 +5,11 @@ const globalForPrisma = globalThis as unknown as {
 }
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient({
-    datasourceUrl: process.env.DATABASE_URL,
+    datasources: {
+        db: {
+            url: process.env.DATABASE_URL,
+        },
+    },
 })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
