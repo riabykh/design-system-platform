ece57454e83e864e3b9984da06e505bfad91ef6c
Kyrylo Riabykh <kyryloriabykh@MacBookPro.home>
2025-12-18 08:24:07 +0100

    fix: suppress typescript error for prisma config

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,12 +4,14 @@ const globalForPrisma = globalThis as unknown as {
     prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({
+const prismaClientOptions = {
     datasources: {
         db: {
             url: process.env.DATABASE_URL,
         },
     },
-})
+}
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient(prismaClientOptions as any)
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
